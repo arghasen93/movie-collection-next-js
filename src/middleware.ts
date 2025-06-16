5ab113e53ea928540f287d9a925208c4e09b40fd
Argha <argha.sen93@gmail.com>
2025-06-16 08:59:42 +0530

    debug logs added

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,14 +3,14 @@ import type { NextRequest } from 'next/server';
 
 export function middleware(request: NextRequest) {
   console.log(JSON.stringify(request.headers));
-  const countryHeader = request.headers.get('X-Country-Code');
+  // const countryHeader = request.headers.get('X-Country-Code');
 
   // Block request if header is missing
-  if (!countryHeader || countryHeader === 'Unknown') {
-    return new NextResponse(`Unauthorized${JSON.stringify(request.headers)}`, {
-      status: 403,
-    });
-  }
+  // if (!countryHeader || countryHeader === 'Unknown') {
+  //   return new NextResponse(`Unauthorized${JSON.stringify(request.headers)}`, {
+  //     status: 403,
+  //   });
+  // }
 
   // Otherwise, let the request continue
   return NextResponse.next();
